ba89d667f09718447d22b021c13e48e2a30c4f2d
linzetai <Linzetaibaylor521@gmail.com>
2023-10-11 20:51:09 +0800

    upload img

--- router/app.go
+++ router/app.go
@@ -36,6 +36,7 @@ func Router() *gin.Engine {
 	// 发送消息
 	r.GET("/user/sendMsg", service.SendMsg)
 	r.GET("/user/sendUserMsg", service.SendUserMsg)
+	r.POST("/attach/upload", service.Upload)
 
 	return r
 }
---
+++ service/attach.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"fmt"
+	"ginchat/utils"
+	"io"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Upload(c *gin.Context) {
+	w := c.Writer
+	req := c.Request
+	srcFile, head, err := req.FormFile("file")
+	if err != nil {
+		utils.RespFail(w, err.Error())
+	}
+	suffix := ".png"
+	oFileName := head.Filename
+	temp := strings.Split(oFileName, ".")
+	if len(temp) > 1 {
+		suffix = "." + temp[len(temp)-1]
+	}
+
+	fileName := fmt.Sprintf("%d%04d%s", time.Now().Unix(), rand.Int31(), suffix)
+	dstFile, err := os.Create("./asset/upload/" + fileName)
+	if err != nil {
+		utils.RespFail(w, err.Error())
+	}
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		utils.RespFail(w, err.Error())
+	}
+
+	url := "./asset/upload/" + fileName
+	utils.RespOk(w, url, "发送图片成功")
+}
